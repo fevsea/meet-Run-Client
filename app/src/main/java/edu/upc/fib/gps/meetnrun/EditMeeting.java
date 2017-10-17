718ddd35af4057fe24b666d915301ad604adade3
Guillem Castro <guillem.castro@hotmail.com>
2017-10-17 19:51:56 +0200

    Added PlacePicker and toolbar Buttons

--- app/src/main/java/edu/upc/fib/gps/meetnrun/EditMeeting.java
+++ app/src/main/java/edu/upc/fib/gps/meetnrun/EditMeeting.java
@@ -4,31 +4,51 @@ import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.FragmentTransaction;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Debug;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.UserRecoverableException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class EditMeeting extends AppCompatActivity implements View.OnClickListener, OnMapReadyCallback {
 
     private Integer year, month, day, hour, minute;
-    private LatLng location = new LatLng(46.5, 45.6);
+    private LatLng location = new LatLng(41.38869, 2.11273);
     private GoogleMap map;
-    private FusedLocationProviderClient mFusedLocationClient;
-
+    private Marker marker;
+    private boolean saved = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +59,17 @@ public class EditMeeting extends AppCompatActivity implements View.OnClickListen
         changeDateButton.setOnClickListener(this);
         Button changeTimeButton = (Button) findViewById(R.id.change_time_button);
         changeTimeButton.setOnClickListener(this);
+        Button changeLocationButton = (Button) findViewById(R.id.change_location_button);
+        changeLocationButton.setOnClickListener(this);
 
         MapFragment mMapFragment = MapFragment.newInstance();
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.map, mMapFragment);
         fragmentTransaction.commit();
-
-        //SupportMapFragment mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
         mMapFragment.getMapAsync(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         this.setTitle("Edit Meeting");
     }
@@ -59,6 +82,9 @@ public class EditMeeting extends AppCompatActivity implements View.OnClickListen
                 break;
             case R.id.change_time_button:
                 showTimePickerDialog();
+                break;
+            case R.id.change_location_button:
+                showLocationPicker();
                 break;
         }
 
@@ -98,14 +124,91 @@ public class EditMeeting extends AppCompatActivity implements View.OnClickListen
         timePickerFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
+    private void showLocationPicker() {
+        int PLACE_PICKER_REQUEST = 1;
+        try {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        }
+        catch (GooglePlayServicesRepairableException|GooglePlayServicesNotAvailableException e) {
+            GooglePlayServicesUtil.getErrorDialog(GooglePlayServicesUtil.isGooglePlayServicesAvailable(this), this, PLACE_PICKER_REQUEST);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
-        map.addMarker(new MarkerOptions().position(location).title("Meeting"));
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            map.setMyLocationEnabled(true);
-        } else {
-            // Show rationale and request permission.
+        marker = map.addMarker(new MarkerOptions().position(location).title("Meeting"));
+        moveMapCameraAndMarker(this.location);
+    }
+
+    private void moveMapCameraAndMarker(LatLng location) {
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(location, 15);
+        map.moveCamera(camera);
+        marker.remove();
+        marker = map.addMarker(new MarkerOptions().position(location).title("Meeting"));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!saved) {
+            createCloseDialog();
+        }
+        else {
+            super.onBackPressed();
         }
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    private void createCloseDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.edit_meeting_close_dialog_title);
+        builder.setMessage(R.string.edit_meeting_close_dialog_message);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditMeeting.super.onBackPressed();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_meeting_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.done_button) {
+            saved = true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) { //Retrieve the result from the PlacePicker
+                Place place = PlacePicker.getPlace(this, data);
+                location = place.getLatLng();
+                moveMapCameraAndMarker(location);
+            }
+        }
+    }
+
+
 }
