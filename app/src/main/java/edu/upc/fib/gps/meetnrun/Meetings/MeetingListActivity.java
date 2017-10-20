d392a69827559e7aa9d7097d15da63b4401f4022
Marc Paricio <paricio.marc@gmail.com>
2017-10-20 09:47:49 +0200

    meeting item actualitzat

--- app/src/main/java/edu/upc/fib/gps/meetnrun/Meetings/MeetingListActivity.java
+++ app/src/main/java/edu/upc/fib/gps/meetnrun/Meetings/MeetingListActivity.java
@@ -1,13 +1,18 @@
 package edu.upc.fib.gps.meetnrun.Meetings;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import edu.upc.fib.gps.meetnrun.R;
 
@@ -20,12 +25,11 @@ public class MeetingListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting_list);
 
-        //TODO setup toolbar and drawer
-        Toolbar toolbar = (Toolbar) findViewById(R.id.action_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.meeting_list_toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        //TODO actionBar.setDisplayHomeAsUpEnabled(true);
-        //TODO actionBar.setHomeAsUpIndicator(R.drawable.);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_18dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_meeting_list_drawer);
         drawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
@@ -59,5 +63,24 @@ public class MeetingListActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meeting_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+            case R.id.meeting_list_menu_search:
+                //TODO search
+                break;
+        }
+        return true;
     }
 }
