726a16139b97ca770d52697963728e116b9341d5
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2017-10-20 09:47:40 +0200

    Added models for User and meeting, added Persistence controllers to add users and meetings, also included the class for error code

---
+++ app/src/main/java/edu/upc/fib/gps/meetnrun/models/Meeting.java
@@ -0,0 +1,93 @@
+package edu.upc.fib.gps.meetnrun.models;
+
+import java.util.Date;
+
+/**
+ * Created by Awais Iqbal on 17/10/2017.
+ */
+
+public class Meeting {
+    private String title;
+    private String description;
+    private String creatorAuthor;
+    private Date dateTime;
+    private boolean isPublic;
+    private int level;
+    private float latitude;
+    private float longitude;
+
+    public Meeting(String title, String description, String creatorAuthor, Date dateTime, boolean isPublic, int level, float latitude, float longitude) {
+        this.title = title;
+        this.description = description;
+        this.creatorAuthor = creatorAuthor;
+        this.dateTime = dateTime;
+        this.isPublic = isPublic;
+        this.level = level;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatorAuthor() {
+        return creatorAuthor;
+    }
+
+    public void setCreatorAuthor(String creatorAuthor) {
+        this.creatorAuthor = creatorAuthor;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
