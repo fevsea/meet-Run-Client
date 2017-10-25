5ccb0d3ca944c2cf733ee2b57a5706bbf510da1b
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2017-10-25 18:02:57 +0200

    changed client timeout to 100 secs

--- app/src/main/java/edu/upc/fib/meetnrun/remote/RetrofitClient.java
+++ app/src/main/java/edu/upc/fib/meetnrun/remote/RetrofitClient.java
@@ -17,8 +17,8 @@ public class RetrofitClient {
     public static Retrofit getClient(String baseUrl) {
         if (retrofit==null) {
             OkHttpClient client = new OkHttpClient.Builder().
-                    connectTimeout(10, TimeUnit.SECONDS)
-                    .readTimeout(10,TimeUnit.SECONDS)
+                    connectTimeout(100, TimeUnit.SECONDS)
+                    .readTimeout(100,TimeUnit.SECONDS)
                     .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
