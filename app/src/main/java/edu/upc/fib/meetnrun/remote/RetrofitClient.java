a65b3459147bc14f03eea841592498d07b3ec872
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2017-10-26 09:34:59 +0200

    fixed error connecting to the server retrying it 10 times

--- app/src/main/java/edu/upc/fib/meetnrun/remote/RetrofitClient.java
+++ app/src/main/java/edu/upc/fib/meetnrun/remote/RetrofitClient.java
@@ -2,7 +2,9 @@ package edu.upc.fib.meetnrun.remote;
 
 import android.util.Log;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.net.ProtocolException;
 import java.util.concurrent.TimeUnit;
 
 import edu.upc.fib.meetnrun.models.CurrentSession;
@@ -27,23 +29,27 @@ public class RetrofitClient {
             HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
                 @Override
                 public void log(String message) {
-                    Log.e("RETROFIT",message);
+                    Log.w("RETROFIT",message);
                 }
             });
             Interceptor retry = new Interceptor() {
+                //Interceptor used to retry request when get some IOException
                 @Override
                 public Response intercept(Chain chain) throws IOException {
                     Request request = chain.request();
-                    // try the request
-                    Response response = chain.proceed(request);
-                    int tryCount = 0;
-                    while (!response.isSuccessful() && tryCount < 4) {
-                        Log.d("intercept", "Request is not successful - " + tryCount);
-                        tryCount++;
-                        // retry the request
+                    Response response = null;
+                    int tryCount = 0, max = 10;
+                    while (tryCount < max) {
+                    try {
                         response = chain.proceed(request);
+                        Log.d("RETOFITCLIENT","Response received from server");
+                        tryCount = max;
+                    } catch (IOException  ioe){
+                        tryCount++;
+                        Log.e("RETOFITCLIENT","IOException happened in the request to server...");
+                        Log.e("RETOFITCLIENT","RETRYING Request...");
                     }
-                    // otherwise just pass the original response on
+                    }
                     return response;
                 }
             };
@@ -52,6 +58,7 @@ public class RetrofitClient {
                     connectTimeout(100, TimeUnit.SECONDS)
                     .readTimeout(100,TimeUnit.SECONDS)
                     .addInterceptor(new AuthenticationInterceptor())
+                    .addInterceptor(retry)
                     .retryOnConnectionFailure(true)
                     .addInterceptor(logging)
                     .build();
