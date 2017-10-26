3f2106b344456603651271fb5bad1e4756b47ee5
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2017-10-26 22:16:22 +0200

    removed extra form

--- app/src/main/java/edu/upc/fib/meetnrun/utils/FormContainers.java
+++ app/src/main/java/edu/upc/fib/meetnrun/utils/FormContainers.java
@@ -84,22 +84,4 @@ public class FormContainers {
         }
     }
 
-    public static class LoginUser{
-        private String username;
-        private String password;
-
-        public LoginUser(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-    }
-
 }
