7a9908b99772e58d64001cac0547d5da20c2c39e
Monica Follana <monicfm44@gmail.com>
2017-11-01 10:31:18 +0100

    fix some bugs

--- app/src/main/java/edu/upc/fib/meetnrun/views/fragments/ProfileActivityFragment.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/ProfileActivityFragment.java
@@ -55,6 +55,10 @@ public class ProfileActivityFragment extends Fragment {
             }
         });
 
+        FloatingActionButton fab =
+                (FloatingActionButton) getActivity().findViewById(R.id.activity_fab);
+        fab.setImageResource(android.R.drawable.ic_menu_edit); fab.setVisibility(View.GONE);
+
 
         return view;
     }
