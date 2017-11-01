21755f3f8a8fdb66f3f935ffb76c7396976b6ac2
Marc Paricio <paricio.marc@gmail.com>
2017-11-01 22:29:28 +0100

    Friends activity and fragment

---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/FriendsActivity.java
@@ -0,0 +1,41 @@
+package edu.upc.fib.meetnrun.views;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import edu.upc.fib.meetnrun.R;
+import edu.upc.fib.meetnrun.views.fragments.FriendsFragment;
+
+public class FriendsActivity extends BaseDrawerActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new FriendsFragment();
+    }
+
+    @Override
+    protected boolean finishOnChangeView() {
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.empty_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/FriendsFragment.java
@@ -0,0 +1,26 @@
+package edu.upc.fib.meetnrun.views.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.upc.fib.meetnrun.R;
+
+
+public class FriendsFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        this.view = view;
+
+
+
+        return this.view;
+    }
+}
