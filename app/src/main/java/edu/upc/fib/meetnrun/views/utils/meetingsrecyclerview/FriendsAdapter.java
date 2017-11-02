7533810d879920da546829f54eccd4892910265e
Eric Rodríguez Balsells <eric@MacBook-Pro-de-Eric.local>
2017-11-02 20:01:39 +0100

    users layout

---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/utils/meetingsrecyclerview/FriendsAdapter.java
@@ -0,0 +1,56 @@
+package edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import edu.upc.fib.meetnrun.R;
+import edu.upc.fib.meetnrun.models.Meeting;
+
+/**
+ * Created by eric on 2/11/17.
+ */
+
+public class FriendsAdapter extends RecyclerView.Adapter<MeetingsViewHolder> {
+
+    private List<Meeting> meetings;
+    private RecyclerViewOnClickListener listener;
+
+    public FriendsAdapter(List<Meeting> meetings, RecyclerViewOnClickListener listener) {
+        this.meetings = meetings;
+        this.listener = listener;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public MeetingsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.meeting_item, parent, false);
+        return new MeetingsViewHolder(view,listener);
+    }
+
+    @Override
+    public void onBindViewHolder(final MeetingsViewHolder holder, int position) {
+        Meeting meeting = meetings.get(position);
+        holder.bindMeeting(meeting);
+    }
+
+    @Override
+    public int getItemCount() {
+        return meetings.size();
+    }
+
+
+    public void updateMeetingsList(List<Meeting> meetings) {
+        this.meetings = meetings;
+        notifyDataSetChanged();
+    }
+
+    public Meeting getMeetingAtPosition(int position) {
+        return meetings.get(position);
+    }
+
+}
---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/utils/meetingsrecyclerview/FriendsViewHolder.java
@@ -0,0 +1,79 @@
+package edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview;
+
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+import edu.upc.fib.meetnrun.R;
+import edu.upc.fib.meetnrun.models.Meeting;
+
+/**
+ * Created by eric on 2/11/17.
+ */
+
+public class FriendsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private View view;
+    private WeakReference<RecyclerViewOnClickListener> listener;
+    private ImageButton addUserButton;
+
+    public FriendsViewHolder(View itemView, RecyclerViewOnClickListener listener) {
+        super(itemView);
+        view = itemView;
+        this.listener = new WeakReference<>(listener);
+    }
+
+    public void bindMeeting(Meeting meeting) {
+        TextView userPhoto = view.findViewById(R.id.meeting_item_user_photo);
+        char letter = meeting.getTitle().charAt(0);
+        String firstLetter = String.valueOf(letter);
+        userPhoto.setBackground(getColoredCircularShape((letter)));
+        userPhoto.setText(firstLetter);
+
+        TextView userName = view.findViewById(R.id.meeting_item_title);
+        userName.setText(meeting.getTitle());
+
+        TextView meetingLocation = view.findViewById(R.id.meeting_item_description);
+        meetingLocation.setText(meeting.getDescription());
+
+        TextView meetingLevel = view.findViewById(R.id.meeting_item_level);
+        String level = String.valueOf(meeting.getLevel());
+        if (level.equals("null")) level = "0";
+        meetingLevel.setText(String.valueOf(level));
+
+        TextView meetingDate = view.findViewById(R.id.meeting_item_date);
+        String datetime = meeting.getDate();
+        meetingDate.setText(datetime.substring(0,datetime.indexOf('T')));
+        TextView meetingTime = view.findViewById(R.id.meeting_item_time);
+        meetingTime.setText(datetime.substring(datetime.indexOf('T')+1,datetime.indexOf('Z')));
+
+        addUserButton = view.findViewById(R.id.meeting_item_meet);
+        addUserButton.setOnClickListener(this);
+
+        view.setOnClickListener(this);
+    }
+
+    private GradientDrawable getColoredCircularShape(char letter) {
+        int[] colors = view.getResources().getIntArray(R.array.colors);
+        GradientDrawable circularShape = (GradientDrawable) ContextCompat.getDrawable(view.getContext(),R.drawable.user_profile_circular_text_view);
+        int position = letter%colors.length;
+        circularShape.setColor(colors[position]);
+        return circularShape;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == addUserButton.getId()) {
+            listener.get().onButtonClicked(getAdapterPosition());
+        }
+        else {
+            listener.get().onMeetingClicked(getAdapterPosition());
+        }
+    }
+
+}
