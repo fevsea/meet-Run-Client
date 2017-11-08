4b39630a3aa136cf97ee318f0386f2e2ba32e096
javier.aleman <javier.aleman@est.fib.upc.edu>
2017-11-08 20:16:10 +0100

    Activity and Fragment for adding friends in a meeting created

---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/MeetingFriendsFragment.java
@@ -0,0 +1,11 @@
+package edu.upc.fib.meetnrun.views.fragments;
+
+/**
+ * Created by Javier on 08/11/2017.
+ */
+
+public class MeetingFriendsFragment {
+    public MeetingFriendsFragment() {
+        super();
+    }
+}
---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/utils/meetingsrecyclerview/MeetingFriendsActivity.java
@@ -0,0 +1,11 @@
+package edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview;
+
+/**
+ * Created by Javier on 08/11/2017.
+ */
+
+public class MeetingFriendsActivity {
+    public MeetingFriendsActivity() {
+        super();
+    }
+}
