9bc043cde8c7617e515799a9e78747346dd198f3
Monica Follana <monicfm44@gmail.com>
2017-11-16 16:33:10 +0100

    code fixings

--- app/src/androidTest/java/edu/upc/fib/meetnrun/ProfileActivityTest.java
+++ app/src/androidTest/java/edu/upc/fib/meetnrun/ProfileActivityTest.java
@@ -1,48 +1,31 @@
 package edu.upc.fib.meetnrun;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.contrib.PickerActions;
-import android.support.test.espresso.matcher.ViewMatchers;
+
+import android.support.test.espresso.intent.Intents;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import edu.upc.fib.meetnrun.models.CurrentSession;
-import edu.upc.fib.meetnrun.views.EditMeetingActivity;
-import edu.upc.fib.meetnrun.views.MainActivity;
-import edu.upc.fib.meetnrun.views.MeetingInfoActivity;
 import edu.upc.fib.meetnrun.views.ProfileActivity;
 import edu.upc.fib.meetnrun.models.User;
+import edu.upc.fib.meetnrun.views.fragments.MeetingListFragment;
+import edu.upc.fib.meetnrun.views.fragments.ProfileActivityFragment;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
-public class UserProfileTest {
+public class ProfileActivityTest {
 
     private User u;
 
@@ -51,21 +34,33 @@ public class UserProfileTest {
             ProfileActivity.class) {
         @Override
         protected void beforeActivityLaunched() {
-            u = new User();
-            u.setId(1);
-            u.setUsername("Monica");
-            u.setFirstName("Monica");
-            u.setLastName("Follana");
-            u.setPostalCode("08028");
-            u.setQuestion("Question");
+            u = new User(1,"MFM","Monica","Follana","08028", "Question", 5);
 
+            CurrentSession.getInstance().setToken("AAAA");
             CurrentSession.getInstance().setCurrentUser(u);
         }
     };
 
+    private ProfileActivityFragment getActivityFragment() {
+        ProfileActivityFragment profileActivityFragment =
+                (ProfileActivityFragment) activityRule.getActivity()
+                        .getSupportFragmentManager()
+                        .findFragmentById(R.id.activity_contentFrame);
+        if (profileActivityFragment == null) {
+            profileActivityFragment = new ProfileActivityFragment();
+            activityRule.getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.activity_contentFrame, profileActivityFragment)
+                    .commit();
+        }
+        return profileActivityFragment;
+    }
+
     @Test
     public void testVisibility() {
-        onView(withId(R.id.fragment)).check(matches(isDisplayed()));
+        Intents.init();
+        ProfileActivityFragment profileActivityFragment = getActivityFragment();
+        onView(withId(R.id.fragment_profile)).check(matches(isDisplayed()));
     }
 
     @Test
