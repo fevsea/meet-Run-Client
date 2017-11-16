03eb32a1ebc7dcb97374fe068c55184e8632a5b9
javier.aleman <javier.aleman@est.fib.upc.edu>
2017-11-16 13:54:49 +0100

    Friends meetings fragment now looks better, but does not work

--- app/src/main/java/edu/upc/fib/meetnrun/views/fragments/MeetingFriendsFragment.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/MeetingFriendsFragment.java
@@ -1,5 +1,6 @@
 package edu.upc.fib.meetnrun.views.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,9 +12,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +30,8 @@ import edu.upc.fib.meetnrun.models.User;
 import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.FriendsAdapter;
 
 import static android.R.layout.simple_list_item_1;
+import static android.R.layout.simple_list_item_activated_2;
+import static android.R.layout.simple_list_item_checked;
 
 
 /**
@@ -41,7 +46,8 @@ public class MeetingFriendsFragment extends Fragment {
     private FragmentActivity context;
 
     private IFriendsAdapter friendsDBAdapter;
-    private List<User> l;
+    List<User> l;
+    List<User> _users;
 
     ListView lv;
 
@@ -59,6 +65,7 @@ public class MeetingFriendsFragment extends Fragment {
         context=this.getActivity();
 
         lv = (ListView) view.findViewById(R.id.myFriends);
+
         Bundle meetingLevel= getActivity().getIntent().getExtras();
         meetingId=meetingLevel.getInt("meetingId");
         level=meetingLevel.getInt("level");
@@ -66,7 +73,36 @@ public class MeetingFriendsFragment extends Fragment {
 
         return view;
     }
+    public class UsersArrayAdapter extends ArrayAdapter<User>{
+        LayoutInflater _li;
+        int _resource;
+        List<User> _users;
 
+        public UsersArrayAdapter(Context context, int resource, List<User> objects) {
+            super(context, resource, objects);
+
+            this._li = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this._resource = resource;
+            this._users = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                convertView = this._li.inflate(this._resource, null);
+            }
+
+            User item = this._users.get(position);
+            TextView tvTitle = (TextView)convertView.findViewById(android.R.id.text1);
+            TextView tvSubtitle = (TextView)convertView.findViewById(android.R.id.text2);
+
+
+            tvTitle.setText(item.getFirstName()+" "+item.getLastName());
+            tvSubtitle.setText(item.getUsername()+" -> "+item.getPostalCode());
+
+            return convertView;
+        }
+    }
     private class getFriends extends AsyncTask<String,String,String> {
 
         @Override
@@ -82,8 +118,22 @@ public class MeetingFriendsFragment extends Fragment {
 
         @Override
         protected void onPostExecute(String s) {
-            ArrayAdapter<User> adapter = new ArrayAdapter<User>(context, simple_list_item_1,l);
-            lv.setAdapter(adapter);
+            ArrayAdapter<User> adapter = new ArrayAdapter<User>(context, simple_list_item_activated_2,l);
+            lv.setAdapter(
+                    new UsersArrayAdapter(
+                            context,
+                            android.R.layout.simple_list_item_activated_2,
+                            l
+                    )
+            );
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> adapter, View v, int pos, long id) {
+                /* The code goes here */
+                    //User item = adapter.getSelectedItem();
+                }
+            });
             super.onPostExecute(s);
         }
     }
