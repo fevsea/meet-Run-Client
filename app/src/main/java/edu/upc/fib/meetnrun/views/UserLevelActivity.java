25475f89be6d2664be1e8b3d9d25418fba05bee7
javier.aleman <javier.aleman@est.fib.upc.edu>
2017-11-23 16:38:22 +0100

    User level started

---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/UserLevelActivity.java
@@ -0,0 +1,8 @@
+package edu.upc.fib.meetnrun.views;
+
+/**
+ * Created by Javier on 23/11/2017.
+ */
+
+public class UserLevelActivity {
+}
---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/UserLevelFragment.java
@@ -0,0 +1,32 @@
+package edu.upc.fib.meetnrun.views.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import edu.upc.fib.meetnrun.R;
+import edu.upc.fib.meetnrun.adapters.IUserAdapter;
+import edu.upc.fib.meetnrun.models.CurrentSession;
+import edu.upc.fib.meetnrun.models.User;
+
+/**
+ * Created by Javier on 23/11/2017.
+ */
+
+public class UserLevelFragment extends Fragment {
+    private View view;
+    private User u;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_level, container, false);
+        this.view = view;
+        u = CurrentSession.getInstance().getCurrentUser();
+        int level=u.getLevel();
+        ProgressBar km=(ProgressBar) view.findViewById(R.id.kmProgressBar);
+        ProgressBar
+        return view;
+    }
+}
