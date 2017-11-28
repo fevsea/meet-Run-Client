5bfcb07247d8ac93daa63b0eaed8be927e1baa7f
Eric Rodríguez Balsells <eric@MacBook-Pro-de-Eric.local>
2017-11-29 00:49:56 +0100

    login see password

--- app/src/main/java/edu/upc/fib/meetnrun/views/LoginActivity.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/LoginActivity.java
@@ -7,9 +7,13 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import edu.upc.fib.meetnrun.R;
@@ -21,10 +25,12 @@ import edu.upc.fib.meetnrun.models.User;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editUsername, editPassword;
+    private TextView seePassword;
     private String username, password;
     public static final String MY_PREFS_NAME = "TokenFile";
     private ILoginAdapter loginAdapter;
     private CurrentSession cs;
+    private boolean see = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,40 @@ public class LoginActivity extends AppCompatActivity {
 
         editUsername = (EditText) findViewById(R.id.editUsername);
         editPassword = (EditText) findViewById(R.id.editPassword);
+        seePassword = (TextView) findViewById(R.id.see_password);
+
+        editPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (editPassword.getText().toString().equals("")) {
+                    seePassword.setVisibility(View.GONE);
+                }
+                else seePassword.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        seePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (see) {
+                    editPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    see = false;
+                }
+                else {
+                    editPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                    see = true;
+                }
+            }
+        });
 
         cs = CurrentSession.getInstance();
         loginAdapter = cs.getLoginAdapter();
