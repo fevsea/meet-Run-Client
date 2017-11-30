1dcc8c57a2119113103cc0dad461cfdd6e8ce6ce
javier.aleman <javier.aleman@est.fib.upc.edu>
2017-11-30 13:50:06 +0100

    layout level and statistics + code level

--- app/src/main/java/edu/upc/fib/meetnrun/views/fragments/StatisticsProfileFragment.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/StatisticsProfileFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import edu.upc.fib.meetnrun.R;
@@ -30,6 +31,9 @@ public class StatisticsProfileFragment extends Fragment {
     TextView minLength;
     TextView maxTime;
     TextView minTime;
+    ProgressBar percentMeetings;
+    ProgressBar percentKm;
+    ProgressBar percentLevel;
     private FragmentActivity context;
     private int userLevel;
 
@@ -49,23 +53,37 @@ public class StatisticsProfileFragment extends Fragment {
         super.onCreate(savedInstanceState);
         page = getArguments().getInt("2", 2);
         title = getArguments().getString("Statistics");
-
+        //TODO: Hallar nivel usuario al que se le mira las estadísticas. Abajo es fake
+        userLevel=0;
 
     }
 
     public void calcLevel (int meetings, float km){
         meetings--;
         for (int i=1; i<userLevel; i++){
-            meetings -= 10*i;
+            meetings -= 10*(i-1);
         }
-        float resMeetings = (float) (meetings/(10*userLevel));
+        float  resMeetings;
+        if (userLevel>0) resMeetings = (float) (meetings/(10*userLevel));
+        else resMeetings = (float) meetings++;
         if (resMeetings>1.0) resMeetings=(float) 1.0;
         float resUser = (float) (km/(10+2.5*(userLevel*userLevel)));
         if (resUser>1.0) resUser=(float) 1.0;
         float res= (float) (0.5*resMeetings+0.5*resUser);
-        if (res>=1.0) {
+        while (res>=1.0) {
+            meetings-=10*(userLevel);
             userLevel++;
+            resMeetings= (float) (meetings/(10*userLevel));
+            if (resMeetings>1.0) resMeetings=(float) 1.0;
+            resUser = (float) (km /(10+2.5*(userLevel*userLevel)));
+            if (resUser>1.0) resUser=(float) 1.0;
+            res= (float) (0.5*resMeetings+0.5*resUser);
         }
+        //TODO: Mirar que el nivel que se quiere poner no sea menor que el que ya tiene (data race)
+        percentMeetings.setProgress ((int) (100*resMeetings));
+        percentKm.setProgress((int) (100*resUser));
+        percentLevel.setProgress((int) (100*res));
+        level.setText (String.valueOf(userLevel));
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -89,10 +107,12 @@ public class StatisticsProfileFragment extends Fragment {
         minTime   = view.findViewById (R.id.minTime);
         maxLength = view.findViewById (R.id.maxLength);
         minLength = view.findViewById (R.id.minLength);
-
+        percentMeetings = view.findViewById (R.id.percentMeetings);
+        percentKm = view.findViewById (R.id.percentKm);
+        percentLevel=view.findViewById(R.id.percentLevel);
 
         username.setText ("Fulanito");
-        level.setText ("50");
+        level.setText (String.valueOf(userLevel));
         meetings.setText("20000");
         steps.setText("1500000");
         totalKm.setText("150");
@@ -106,6 +126,7 @@ public class StatisticsProfileFragment extends Fragment {
         maxLength.setText("2.025");
         minLength.setText("0.015");
         minTime.setText("0h25m30s");
+        calcLevel(20000,150);
         return view;
     }
 }
