986840842e8c45a04ec4e504aae266efff2451ff
Alejandro Agustin B <alejandro.a.es@ieee.org>
2017-12-01 14:19:26 +0100

    notification example

--- app/src/main/java/edu/upc/fib/meetnrun/services/FirebaseMsgService.java
+++ app/src/main/java/edu/upc/fib/meetnrun/services/FirebaseMsgService.java
@@ -1,12 +1,21 @@
 package edu.upc.fib.meetnrun.services;
 
-import android.app.Service;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import edu.upc.fib.meetnrun.R;
 
 public class FirebaseMsgService extends FirebaseMessagingService {
 
@@ -14,7 +23,7 @@ public class FirebaseMsgService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        String json = null;
+        Map<String, String> data = null;
         String type = null;
 
         // TODO(developer): Handle FCM messages here.
@@ -29,17 +38,34 @@ public class FirebaseMsgService extends FirebaseMessagingService {
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            json = remoteMessage.getData().toString();
-            Log.d(TAG, "Message data payload: " + json);
+            data = remoteMessage.getData();
+            Log.d(TAG, "Message data payload: " + data.toString());
         }
 
-        consumeMessage(type, json);
+
+
+        consumeMessage(type, data);
     }
 
-    public void consumeMessage(String type, String json){
+    public void consumeMessage(String type, Map<String, String> data){
         switch (type) {
+            case "notification":
+                issueNotification(data.get("title"), data.get("text"));
+                break;
             default:
                 Log.w(TAG, "UNINPLEMENTED: " +type);
         }
+    }
+
+    public void issueNotification(String title, String text) {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle(title)
+                        .setSmallIcon(R.drawable.help)
+                        .setContentText(text);
+
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, mBuilder.build());
     }
 }
