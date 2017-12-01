bd74e56501ad79312d58eab4aa926b1126236289
Monica Follana <monicfm44@gmail.com>
2017-12-01 15:01:43 +0100

    past meeting info view working (hardcoded)

---
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/PastMeetingInfoFragment.java
@@ -0,0 +1,246 @@
+package edu.upc.fib.meetnrun.views.fragments;
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.upc.fib.meetnrun.R;
+import edu.upc.fib.meetnrun.adapters.IFriendsAdapter;
+import edu.upc.fib.meetnrun.adapters.IMeetingAdapter;
+import edu.upc.fib.meetnrun.exceptions.AutorizationException;
+import edu.upc.fib.meetnrun.exceptions.ParamsException;
+import edu.upc.fib.meetnrun.models.CurrentSession;
+import edu.upc.fib.meetnrun.models.User;
+import edu.upc.fib.meetnrun.views.EditMeetingActivity;
+import edu.upc.fib.meetnrun.views.FriendProfileActivity;
+import edu.upc.fib.meetnrun.views.ProfileViewPagerFragment;
+import edu.upc.fib.meetnrun.views.UserProfileActivity;
+import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.FriendsAdapter;
+import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.RecyclerViewOnClickListener;
+
+public class PastMeetingInfoFragment extends Fragment //implements OnMapReadyCallback
+         {
+    private View view;
+    private LatLng location;
+    private GoogleMap map;
+    private Marker marker;
+    private FriendsAdapter participantsAdapter;
+    private IMeetingAdapter meetingController;
+    private IFriendsAdapter friendsController;
+    private List<User> friends;
+    private int meetingId;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_past_meeting_info,container,false);
+        this.view = view;
+
+        meetingController = CurrentSession.getInstance().getMeetingAdapter();
+        friendsController = CurrentSession.getInstance().getFriendsAdapter();
+        Bundle pastMeetingInfo = getActivity().getIntent().getExtras();
+
+        TextView title = view.findViewById(R.id.meeting_info_title);
+        TextView level = view.findViewById(R.id.meeting_info_level);
+        TextView description = view.findViewById(R.id.meeting_info_description);
+        TextView date = view.findViewById(R.id.meeting_info_date);
+        TextView time = view.findViewById(R.id.meeting_info_time);
+        TextView owner = view.findViewById(R.id.meeting_info_creator);
+
+        TextView distance = view.findViewById(R.id.meeting_info_distance);
+        TextView steps = view.findViewById(R.id.meeting_info_steps);
+        TextView totalTime = view.findViewById(R.id.meeting_info_totaltime);
+        TextView avSpeed = view.findViewById(R.id.meeting_info_avspeed);
+        TextView calories = view.findViewById(R.id.meeting_info_calories);
+
+
+        meetingId = pastMeetingInfo.getInt("id");
+
+        title.setText(pastMeetingInfo.getString("title"));
+        owner.setText(pastMeetingInfo.getString("owner"));
+        description.setText(pastMeetingInfo.getString("description"));
+        String levelValue = pastMeetingInfo.getString("level");
+        if(levelValue.equals("null")) levelValue = "0";
+        level.setText(levelValue);
+        date.setText(pastMeetingInfo.getString("date"));
+        time.setText(pastMeetingInfo.getString("time"));
+
+        String distanceValue = pastMeetingInfo.getString("distance");
+        if(distance.equals("null")) distanceValue = "123";
+        distance.setText(distanceValue);
+        steps.setText(pastMeetingInfo.getString("steps"));
+        totalTime.setText(pastMeetingInfo.getString("totaltime"));
+        avSpeed.setText(pastMeetingInfo.getString("avspeed"));
+        calories.setText(pastMeetingInfo.getString("calories"));
+
+
+        setupRecyclerView();
+        setupScrollView();
+        /*location = new LatLng(Double.parseDouble(pastMeetingInfo.getString("latitude")),Double.parseDouble(meetingInfo.getString("longitude")));
+
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.meeting_info_map, mapFragment)
+                .commit();
+        mapFragment.getMapAsync(this);
+        TODO pintar punts al mapa ??*/
+
+        return view;
+    }
+
+    private void setupRecyclerView() {
+
+        final RecyclerView friendsList = view.findViewById(R.id.fragment_friends_container);
+        friendsList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        List<User> users = new ArrayList<>();
+        getParticipantsList();
+
+        participantsAdapter = new FriendsAdapter(users, new RecyclerViewOnClickListener() {
+            @Override
+            public void onButtonClicked(int position) {}
+
+            @Override
+            public void onItemClicked(int position) {
+                User participant = participantsAdapter.getFriendAtPosition(position);
+                Intent profileIntent;
+                if (participant.getId().equals(CurrentSession.getInstance().getCurrentUser().getId())) {
+                    profileIntent = new Intent(getActivity(),ProfileViewPagerFragment.class);
+                }
+                else {
+                    boolean isFriend = false;
+                    for (User friend : friends) {
+                        if (participant.getId().equals(friend.getId())) isFriend = true;
+                    }
+                    if (isFriend) {
+                        profileIntent = new Intent(getActivity(), FriendProfileActivity.class);
+                    }
+                    else {
+                        profileIntent = new Intent(getActivity(), UserProfileActivity.class);
+                    }
+                    profileIntent.putExtra("id",participant.getId().toString());
+                    profileIntent.putExtra("userName", participant.getUsername());
+                    String name = participant.getFirstName() + " " + participant.getLastName();
+                    profileIntent.putExtra("name", name);
+                    profileIntent.putExtra("postCode", participant.getPostalCode());
+                }
+                startActivity(profileIntent);
+
+            }
+        }, getContext(), false);
+        friendsList.setAdapter(participantsAdapter);
+    }
+
+    @Override
+    public void onResume() {
+        getParticipantsList();
+        super.onResume();
+    }
+
+    private void setupScrollView() {
+        final ScrollView scroll = view.findViewById(R.id.meeting_info_scroll);
+        ImageView transparent = view.findViewById(R.id.meeting_info_imagetrans);
+        transparent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        scroll.requestDisallowInterceptTouchEvent(true);
+                        return false;
+
+                    case MotionEvent.ACTION_UP:
+                        scroll.requestDisallowInterceptTouchEvent(false);
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        scroll.requestDisallowInterceptTouchEvent(true);
+                        return false;
+
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+
+    private void getParticipantsList() {
+        new PastMeetingInfoFragment.getParticipants().execute(meetingId);
+        new PastMeetingInfoFragment.getFriends().execute(CurrentSession.getInstance().getCurrentUser().getId().toString());
+    }
+
+    private class getParticipants extends AsyncTask<Integer,String,String> {
+
+        private List<User> l = new ArrayList<>();
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            //TODO handle exceptions
+            try {
+                l = meetingController.getParticipantsFromMeeting(integers[0],0);//TODO arreglar paginas
+            } catch (AutorizationException | ParamsException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            participantsAdapter.updateFriendsList(l);
+            super.onPostExecute(s);
+        }
+    }
+
+    private class getFriends extends AsyncTask<String,String,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                friends = friendsController.getUserFriends(0); //TODO arreglar paginas
+            } catch (AutorizationException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+   /* @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        marker = map.addMarker(new MarkerOptions().position(location).title("Meeting"));
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(location,15);
+        map.moveCamera(camera);
+        marker.remove();
+        marker = map.addMarker(new MarkerOptions().position(location).title("Meeting"));
+    }*/
+}
--- app/src/main/java/edu/upc/fib/meetnrun/views/fragments/PastMeetingsProfileFragment.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/PastMeetingsProfileFragment.java
@@ -20,11 +20,14 @@ import edu.upc.fib.meetnrun.R;
 import edu.upc.fib.meetnrun.adapters.IMeetingAdapter;
 import edu.upc.fib.meetnrun.adapters.IUserAdapter;
 import edu.upc.fib.meetnrun.exceptions.AutorizationException;
+import edu.upc.fib.meetnrun.exceptions.NotFoundException;
 import edu.upc.fib.meetnrun.exceptions.ParamsException;
 import edu.upc.fib.meetnrun.models.CurrentSession;
 import edu.upc.fib.meetnrun.models.Meeting;
+import edu.upc.fib.meetnrun.models.TrackingData;
 import edu.upc.fib.meetnrun.models.User;
 import edu.upc.fib.meetnrun.views.MeetingInfoActivity;
+import edu.upc.fib.meetnrun.views.PastMeetingInfoActivity;
 import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.MeetingsAdapter;
 import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.RecyclerViewOnClickListener;
 
@@ -33,12 +36,15 @@ public class PastMeetingsProfileFragment extends Fragment {
     private IUserAdapter userController;
     private IMeetingAdapter meetingController;
     private int userId;
+    private int meetingId;
     private MeetingsAdapter meetingsAdapter;
 
 
     private View view;
     private SwipeRefreshLayout swipeRefreshLayout;
     private List<Meeting> meetings;
+
+    private TrackingData tracking;
 
     private String title;
     private int page;
@@ -95,26 +101,41 @@ public class PastMeetingsProfileFragment extends Fragment {
 
             @Override
             public void onButtonClicked(int position) {
-                //Meeting selectedMeeting = meetingsAdapter.getMeetingAtPosition(position);
             }
 
             @Override
             public void onItemClicked(int position) {
                 Toast.makeText(view.getContext(), "Showing selected meeting info", Toast.LENGTH_SHORT).show();
                 Meeting meeting = meetingsAdapter.getMeetingAtPosition(position);
-                Intent meetingInfoIntent = new Intent(getActivity(),MeetingInfoActivity.class);
-                meetingInfoIntent.putExtra("id",meeting.getId());
-                meetingInfoIntent.putExtra("title",meeting.getTitle());
-                meetingInfoIntent.putExtra("owner",meeting.getOwner().getUsername());
-                meetingInfoIntent.putExtra("ownerId",meeting.getOwner().getId());
-                meetingInfoIntent.putExtra("description",meeting.getDescription());
+                meetingId = meeting.getId();
+                getTrackingData();
+
+                Intent pastMeetingInfoIntent = new Intent(getActivity(),PastMeetingInfoActivity.class);
+
+                pastMeetingInfoIntent.putExtra("id",meeting.getId());
+                pastMeetingInfoIntent.putExtra("title",meeting.getTitle());
+                pastMeetingInfoIntent.putExtra("owner",meeting.getOwner().getUsername());
+                pastMeetingInfoIntent.putExtra("ownerId",meeting.getOwner().getId());
+                pastMeetingInfoIntent.putExtra("description",meeting.getDescription());
                 String datetime = meeting.getDate();
-                meetingInfoIntent.putExtra("date",datetime.substring(0,datetime.indexOf('T')));
-                meetingInfoIntent.putExtra("time",datetime.substring(datetime.indexOf('T')+1,datetime.indexOf('Z')));
-                meetingInfoIntent.putExtra("level",String.valueOf(meeting.getLevel()));
-                meetingInfoIntent.putExtra("latitude",meeting.getLatitude());
-                meetingInfoIntent.putExtra("longitude",meeting.getLongitude());
-                startActivity(meetingInfoIntent);
+                pastMeetingInfoIntent.putExtra("date",datetime.substring(0,datetime.indexOf('T')));
+                pastMeetingInfoIntent.putExtra("time",datetime.substring(datetime.indexOf('T')+1,datetime.indexOf('Z')));
+                pastMeetingInfoIntent.putExtra("level",String.valueOf(meeting.getLevel()));
+
+
+                pastMeetingInfoIntent.putExtra("distance", "123");
+                pastMeetingInfoIntent.putExtra("steps", "123");
+                pastMeetingInfoIntent.putExtra("totaltime", "123");
+                pastMeetingInfoIntent.putExtra("avspeed", "123");
+                pastMeetingInfoIntent.putExtra("calories", "123");
+
+                /*pastMeetingInfoIntent.putExtra("distance", tracking.getDistance());
+                pastMeetingInfoIntent.putExtra("steps", tracking.getSteps());
+                pastMeetingInfoIntent.putExtra("totaltime", tracking.getTotalTimeMillis());
+                pastMeetingInfoIntent.putExtra("avspeed", tracking.getAverageSpeed());
+                pastMeetingInfoIntent.putExtra("calories", tracking.getCalories());*/
+
+                startActivity(pastMeetingInfoIntent);
 
             }
         });
@@ -123,12 +144,16 @@ public class PastMeetingsProfileFragment extends Fragment {
 
     }
 
+    private void getTrackingData() {
+        new getPastMeetingsTracking().execute(userId, meetingId);
+    }
+
     private void updateMeetingList() {
         new PastMeetingsProfileFragment.GetPastMeetings().execute(userId);
     }
 
 
-    private class GetPastMeetings extends AsyncTask<Integer,String,String> {
+    private class GetPastMeetings extends AsyncTask<Integer,Integer,String> {
         List<Meeting> l = new ArrayList<>();
 
         @Override
@@ -143,7 +168,7 @@ public class PastMeetingsProfileFragment extends Fragment {
             return null;
         }
 
-            @Override
+        @Override
         protected void onPostExecute(String s) {
             System.err.println("FINISHED");
             meetingsAdapter.updateMeetingsList(l);
@@ -152,9 +177,27 @@ public class PastMeetingsProfileFragment extends Fragment {
     }
 
 
-        protected void onPostExecute(String s) {
-            System.err.println("FINISHED");
-            updateMeetingList();
+    private class getPastMeetingsTracking extends AsyncTask<Integer, Integer, String> {
+            @Override
+            protected String doInBackground(Integer... integers) {
+                try {
+                    tracking = meetingController.getTracking(integers[0], integers[1]);
+                } catch (AutorizationException e) {
+                    e.printStackTrace();
+                } catch (NotFoundException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                System.err.println("FINISHED");
+                super.onPostExecute(s);
+            }
         }
+
+
     }
+
 
