bf3a4b4fc2b5e26f89447abab2bb70bab8439b89
Marc Paricio <paricio.marc@gmail.com>
2017-12-01 13:58:51 +0100

    open profile intent

--- app/src/main/java/edu/upc/fib/meetnrun/views/fragments/ChatGroupInfoFragment.java
+++ app/src/main/java/edu/upc/fib/meetnrun/views/fragments/ChatGroupInfoFragment.java
@@ -2,6 +2,7 @@ package edu.upc.fib.meetnrun.views.fragments;
 
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -13,15 +14,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 import edu.upc.fib.meetnrun.R;
 import edu.upc.fib.meetnrun.adapters.IFriendsAdapter;
+import edu.upc.fib.meetnrun.exceptions.AutorizationException;
 import edu.upc.fib.meetnrun.models.Chat;
 import edu.upc.fib.meetnrun.models.CurrentSession;
 import edu.upc.fib.meetnrun.models.User;
 import edu.upc.fib.meetnrun.views.FriendProfileActivity;
+import edu.upc.fib.meetnrun.views.UserProfileActivity;
 import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.FriendsAdapter;
 import edu.upc.fib.meetnrun.views.utils.meetingsrecyclerview.RecyclerViewOnClickListener;
 
@@ -36,6 +42,11 @@ public class ChatGroupInfoFragment extends Fragment {
     private TextView groupName;
     private RecyclerView recyclerView;
     private Chat chat;
+    private List<User> friends;
+    private ProgressBar progressBar;
+    private boolean isLastPage;
+    private boolean isLoading;
+    private int pageNumber;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,6 +68,7 @@ public class ChatGroupInfoFragment extends Fragment {
         groupImage.setBackground(getColoredCircularShape(name.charAt(0)));
         groupImage.setText(String.valueOf(name.charAt(0)));
         groupName = view.findViewById(R.id.group_name);
+        progressBar = view.findViewById(R.id.pb_loading_friends);
         groupName.setText(name);
         TextView groupNumberUsers = view.findViewById(R.id.group_num_users);
         groupNumberUsers.setText(numberOfUsersText);
@@ -70,6 +82,7 @@ public class ChatGroupInfoFragment extends Fragment {
         setupRecyclerView();
         fab = getActivity().findViewById(R.id.activity_fab);
         fab.setVisibility(View.INVISIBLE);
+        new GetAllFriends().execute();
         return this.view;
     }
 
@@ -100,9 +113,20 @@ public class ChatGroupInfoFragment extends Fragment {
     }
 
     protected void getIntent(User friend) {
-        Intent friendProfileIntent = new Intent(getActivity(),FriendProfileActivity.class);
-        startActivity(friendProfileIntent);
-        //TODO check if its friend or not to load different actitivy
+        if (isLoading) Toast.makeText(getContext(),R.string.loading_list,Toast.LENGTH_LONG).show();
+        else {
+            Intent userProfileIntent = null;
+            if (isFriend(friend)) userProfileIntent = new Intent(getActivity(), FriendProfileActivity.class);
+            else userProfileIntent = new Intent(getActivity(), UserProfileActivity.class);
+            startActivity(userProfileIntent);
+        }
+    }
+
+    private boolean isFriend(User friend) {
+        for (User user : friends) {
+            if (user.getId().equals(friend.getId())) return true;
+        }
+        return false;
     }
 
     private GradientDrawable getColoredCircularShape(char letter) {
@@ -111,6 +135,44 @@ public class ChatGroupInfoFragment extends Fragment {
         int position = letter%colors.length;
         circularShape.setColor(colors[position]);
         return circularShape;
+    }
+
+    private class GetAllFriends extends AsyncTask<String,String,String> {
+
+        @Override
+        protected void onPreExecute() {
+            friends = new ArrayList<>();
+            progressBar.setVisibility(View.VISIBLE);
+            isLoading = true;
+            pageNumber = 0;
+            isLastPage = false;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                List<User> friendsPage = new ArrayList<>();
+                while (!isLastPage) {
+                    friendsPage = friendsDBAdapter.getUserFriends(pageNumber);
+                    if (friendsPage.size() != 0) {
+                        friends.addAll(friendsPage);
+                        ++pageNumber;
+                    }
+                    else isLastPage = true;
+                }
+            } catch (AutorizationException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            isLoading = false;
+            progressBar.setVisibility(View.INVISIBLE);
+            super.onPostExecute(s);
+        }
+
     }
 
 }
