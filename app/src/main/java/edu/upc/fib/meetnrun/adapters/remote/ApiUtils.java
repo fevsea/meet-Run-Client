cf1d1a1a4416825b9992562257274f5391d07079
Javier <javieralemancolorado@gmail.com>
2018-01-11 17:49:50 +0100

    changed port to 80

--- app/src/main/java/edu/upc/fib/meetnrun/adapters/remote/ApiUtils.java
+++ app/src/main/java/edu/upc/fib/meetnrun/adapters/remote/ApiUtils.java
@@ -7,7 +7,7 @@ package edu.upc.fib.meetnrun.adapters.remote;
 public class ApiUtils {
 
 
-  public static final String BASE_URL = "http://10.4.41.144:8000/";
+  public static final String BASE_URL = "http://10.4.41.144:80/";
 
   public static SOServices getSOService() {
     return RetrofitClient.getClient(BASE_URL).create(SOServices.class);
