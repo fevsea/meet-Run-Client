0a9ead3c60563245d6f8e4b48d1656d2dec6b23f
Awais Iqbal Begum <awais.iqbal2@gmail.com>
2018-01-11 13:53:28 +0100

    fixed crash in trophies

--- app/src/main/java/edu/upc/fib/meetnrun/adapters/remote/SOServices.java
+++ app/src/main/java/edu/upc/fib/meetnrun/adapters/remote/SOServices.java
@@ -9,9 +9,10 @@ import edu.upc.fib.meetnrun.adapters.models.FriendServer;
 import edu.upc.fib.meetnrun.adapters.models.MeetingServer;
 import edu.upc.fib.meetnrun.adapters.models.PageServer;
 import edu.upc.fib.meetnrun.adapters.models.PositionServer;
+import edu.upc.fib.meetnrun.adapters.models.PositionUserServer;
 import edu.upc.fib.meetnrun.adapters.models.StatisticsServer;
 import edu.upc.fib.meetnrun.adapters.models.TrackServer;
-import edu.upc.fib.meetnrun.adapters.models.PositionUserServer;
+import edu.upc.fib.meetnrun.adapters.models.TrophiesListServer;
 import edu.upc.fib.meetnrun.adapters.models.UserServer;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -75,6 +76,9 @@ public interface SOServices {
 
     @GET("/users/{id}/statistics")
     Call<StatisticsServer> getUserStatisticsByID(@Path("id") int id);
+
+    @GET("/trophies/{id}")
+    Call<TrophiesListServer> getTrophiesListByID(@Path("id") int id);
 
     @POST("/users/{id}/ban")
     Call<Void> requestBan(@Path("id") int id);
